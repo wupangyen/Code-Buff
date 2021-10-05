bd8284f4ee408cfa6ebcc55b30271f92045829b3
wupangyen <72144016+wupangyen@users.noreply.github.com>
2021-10-04 22:29:43 -0400

    Update Kids With the Greatest Number of Candies.java

--- LeetCode-Oct-2021/Kids With the Greatest Number of Candies.java
+++ LeetCode-Oct-2021/Kids With the Greatest Number of Candies.java
@@ -1,1 +1,29 @@
 
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        // time O(n)
+        //space O(n)
+        
+        int max = 0;;
+        List<Boolean> res = new ArrayList<Boolean>();
+        
+        for(int candy: candies){
+            max = Math.max(max,candy);
+        }
+        
+        
+        for(int candy: candies){
+            if(candy + extraCandies >= max){
+                res.add(true);
+            }
+            else{
+                res.add(false);
+            }
+            
+        }
+        
+        return res;
+        
+    }
+}
