f51a09130f8ce91eeb853ce609367563df657088
wupangyen <72144016+wupangyen@users.noreply.github.com>
2021-10-04 23:56:20 -0400

    Update NumberofGoodPairs.java

--- LeetCode-Oct-2021/NumberofGoodPairs.java
+++ LeetCode-Oct-2021/NumberofGoodPairs.java
@@ -1,1 +1,53 @@
+// brute force
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+        int res = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                
+                if(i < j && nums[i] == nums[j]){
+                    res +=1;
+                }
+            }
+            
+        }
+        
+        return res;
+        
+    }
+}
 
+// O(n) time complexity 
+// use combinatorics 
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        // time O(n)
+        // space O(1)
+        int res = 0;
+        int[] freq = new int [101];
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            freq[nums[i]] ++;
+        }
+        
+        for(int f: freq){
+            res += comb(f);
+        }
+        
+        return res;
+        
+        
+    }
+    
+    public int comb(int freq){
+        int res = 0;
+        
+        res = ((freq -1) * freq)/2;
+        
+        return res;
+    }
+}
